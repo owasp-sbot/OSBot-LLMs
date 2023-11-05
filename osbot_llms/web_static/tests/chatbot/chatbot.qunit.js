e6f2b7af5bb050f72507367453f85490ef4abc4e
Dinis Cruz <dinis.cruz@owasp.org>
2023-11-05 11:24:56 +0000

    refactored chatbot.qunit.js to load the page on QUnit module start (i.e. using hooks.before)

--- osbot_llms/web_static/tests/chatbot/chatbot.qunit.js
+++ osbot_llms/web_static/tests/chatbot/chatbot.qunit.js
@@ -1,70 +1,48 @@
 QUnit.module('sample test', function(hooks) {
 
-    // QUnit.test('wallaby load', function  (assert) {
-    //     var done = assert.async();
-    //     //assert.expect(0)
-    //     //let path_file = '/static/src/chatbot/script.js'
-    //     let path_file = '/osbot_llms/web_static/src/chatbot/script.js'
-    //     console.log(path_file)
-    //     $.get(path_file, function(response, script, xhr) {
-    //         console.log(xhr.status)
-    //         assert.equal(xhr.status    , 200)
-    //         console.log(response)
-    //         done()
-    //     })
-        //done()
-    //});
-    QUnit.test('confirm that chatbot.html and script.js loads ok',  function (assert) {
+    hooks.before(function(assert) {
+        //console.log('before test execution')
         var done = assert.async();
+        var this_closure=this
         var virtual_path = "/static/src/chatbot"                          // this works in qunit in browser at http://localhost:8000/static-tests/chatbot/test-chatbot.html
         var page_path = `${virtual_path}/chatbot.html`
-        var div_id    = 'chatbot_div'
-        $chatbot_div  = $(`<div id='${div_id}'>`)
-        $chatbot_div.appendTo('body')
-        assert.ok($(`#${div_id}`).is( $chatbot_div))
 
+        this.div_id    = 'chatbot_div'
+        this.$chatbot_div  = $(`<div id='${this.div_id}'>`)
+        this.$chatbot_div.appendTo('body')
+        //assert.ok($(`#${div_id}`).is( this.$chatbot_div))
 
-        $chatbot_div.load(page_path, function(response, status, xhr) {
-            assert.equal(xhr.status    , 200)
-            assert.equal(xhr.statusText, 'OK')
-            assert.notEqual($chatbot_div.html(),'')
+        this.$chatbot_div.load(page_path, function(response, status, xhr) {
+            this_closure.response     = response
+            this_closure.status       = status
+            this_closure.xhr          = xhr
 
+            console.log(`[hooks.before]: page loaded ok ${page_path}`)
+            done()
+        })
 
-            //$('<div id=chatbot_div>').load(page_path).appendTo('body')
+     })
 
-            $chatbot_div.show()
+    hooks.after(function(assert) {
+         console.log('[hooks.after]: before after test execution')
+     })
 
-            expected_script_js_vars = { chatbox         : {}     ,
-                                        chatInput       : {}     ,
-                                        sendChatBtn     : {}     ,
-                                        userMessage     : null   ,
-                                        API_KEY         : '.....',
-                                        inputInitHeight : window.script_js_vars.inputInitHeight }
-            assert.propEqual(window.script_js_vars, expected_script_js_vars)
+    QUnit.test('check chatbot.html was loaded ok',  function (assert) {
+        assert.equal   (this.xhr.status         , 200 , "xhr.status is 200"            )
+        assert.equal   (this.xhr.statusText     , 'OK', 'xhr.statusText is OK'         )
+        assert.notEqual(this.$chatbot_div.html(), ''  , 'chatbot_div.html is not empty')
+        assert.ok($(`#${this.div_id}`).is( this.$chatbot_div), `div_id ${this.div_id} is in the DOM`)
 
-            //$chatbot_div.remove()     // use this to delete the $chatbot_div from the page
-            done(); // Tells QUnit that you're done with async work
-        });
-
+        expected_script_js_vars = { chatbox         : {}     ,
+                                    chatInput       : {}     ,
+                                    sendChatBtn     : {}     ,
+                                    userMessage     : null   ,
+                                    API_KEY         : '.....',
+                                    inputInitHeight : window.script_js_vars.inputInitHeight }
+        assert.propEqual(window.script_js_vars, expected_script_js_vars)
     })
 
-
-        // // This callback function will be executed after the load completes
-        //     console.log("status: " + status);
-        //
-        //         if (status === "error") {
-        //             console.error("Error loading chatbot.html:", xhr.statusText);
-        //         } else {
-        //             // Log and assert only after the content has been loaded
-        //             console.log($chatbot_div.html());
-        //             // Perform your assertions here, for example:
-        //             // assert.ok($chatbot_div.html().includes("expected content"), "The content should be loaded");
-        //
-        //             // Place your debug code here, or step through with a debugger
-        //         }
-        //
-
-    QUnit.test('sending message', function (assert)   {
+    QUnit.test('sending message',  function (assert)  {
         assert.expect(0)
         chatbox        = document.querySelector(".chatbox");
         // console.log(chatbox)
@@ -83,7 +61,7 @@ QUnit.module('sample test', function(hooks) {
 
 
 QUnit.done(function() {
-  console.log('all tests are done, expanding all tests to show messages')
+  console.log('[QUnit.done]: all tests are done, expanding all tests to show messages')
   document.querySelectorAll('.qunit-assert-list').forEach(function(element) {
     element.classList.remove('qunit-collapsed');
 
