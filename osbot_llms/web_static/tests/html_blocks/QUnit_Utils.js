3d2d650f30f9dd9ecbf86173d268df1d31c47992
Dinis Cruz <dinis.cruz@owasp.org>
2023-11-07 00:15:21 +0000

    Improved JS Div class

--- osbot_llms/web_static/tests/html_blocks/QUnit_Utils.js
+++ osbot_llms/web_static/tests/html_blocks/QUnit_Utils.js
@@ -1,8 +1,76 @@
 export class Div {
     constructor() {
-        // Initialization code here for Div class
+        this.styles     = this.default_styles()
+        this.id         = null
+        this.tag_name   = 'Div'
     }
-    // Additional methods or properties for Div class
+
+    add_to(selector) {
+        const targetElements = document.querySelectorAll(selector);
+        targetElements.forEach(element => {
+            this.add_to_element(element)
+        });
+        return this;
+    }
+    add_to_element(element) {
+        element.insertAdjacentHTML('beforeend', this.html());
+    }
+
+    default_styles() { return { background_color: null,
+                                border          : null,
+                                bottom          : null,
+                                left            : null,
+                                margin          : null,
+                                overflow        : null,
+                                padding         : null,
+                                position        : null,
+                                right           : null,
+                                top             : null,
+                                width           : null,
+                                z_index         : null}}
+    inner_html() { return '' }
+
+    html() {
+        let html = `<${this.tag_name}`;
+        let styleString = '';
+
+        // Check if `this.style` exists and has properties
+        if (this.styles && Object.keys(this.styles).length) {
+            // Construct the style attribute value
+            console.log(this.styles)
+            for (const key in this.styles) {
+                if (this.styles.hasOwnProperty(key) && this.styles[key]) {
+                    const styleKey = key.replace(/([A-Z])/g, '-$1').toLowerCase();      // Convert camelCase to kebab-case for CSS properties
+                    styleString += `${styleKey}: ${this.styles[key]}; `;
+                }
+            }
+
+            // Trim the last space and append the style attribute to the HTML string
+            styleString = styleString.trim();
+            if (styleString.length > 0) {
+                html += ` style="${styleString}"`;
+            }
+        }
+
+        // Add other attributes here if needed...
+
+        // Close the opening tag, insert inner HTML, and close the tag
+        html += `>${this.inner_html()}</${this.tag_name}>`;
+        return html;
+    }
+
+    set_style(key, value) {
+        this.styles[key] = value
+        return this
+    }
+
+    set_styles(key_values) {
+        for (var key in key_values) {
+            const value = key_values[key]
+            this.set_style(key,value)
+        }
+        return this
+    }
 }
 
 export class QUnit_Utils {
--- osbot_llms/web_static/tests/html_blocks/dynamic_cols_rows.qunit.js
+++ osbot_llms/web_static/tests/html_blocks/dynamic_cols_rows.qunit.js
@@ -1,11 +1,86 @@
-import {QUnit_Utils} from './QUnit_Utils.js';
+import {Div, QUnit_Utils} from './QUnit_Utils.js';
 
 QUnit.module('dynamic_cols_rows.html', function(hooks) {
 
     hooks.before(function (assert) {
         this.qunit_utils = new QUnit_Utils()
-        this.div = new QUnit_Utils()
+        window.Div = Div
+        //this.div = new QUnit_Utils()
     })
+
+    // working test (which is the only work that works in real-time in Wallaby)
+    QUnit.test('Div.add_to', function (assert) {
+        const div_id = 'an_id'
+        const target = $(`<div id='${div_id}'>`)[0]
+        assert.equal(target.outerHTML, `<div id="${div_id}"></div>`)
+        console.log(document.body.insertAdjacentHTML('beforeend', target.outerHTML))
+
+
+        const div = new Div();
+        div.set_style('top', '10px');
+        div.set_style('border', '2px solid');
+        const expectedHtml = '<div style="border: 2px solid; top: 10px;"></div>';        // Expected HTML result
+
+        assert.equal($(`#${div_id}`).html(), '')
+        div.add_to(`#${div_id}`);
+
+        const actualHtml = $(`#${div_id}`).html()                                       // Get the value of the id from the DOM
+        console.log(actualHtml)
+        assert.strictEqual(actualHtml, expectedHtml, "The div's HTML should match the expected HTML and be appended to the body");   // Test whether the div was added to the fixture
+
+        // Clean up by removing the added div
+        $(`#${div_id}`).remove()
+
+        assert.equal($(`#${div_id}`).html(), undefined)
+    });
+
+    QUnit.test('Div.default_styles()',  function (assert) {
+        const div = new Div();
+
+        const random_styles = {};                                                          // Generate random values for attributes and set them
+        Object.keys(div.default_styles()).forEach((attr) => {
+            random_styles[attr] = Math.random().toString(36).substring(2, 15);       // Generate a random string for each attribute
+        });
+        div.set_styles(random_styles)
+
+        let expected_styles = ""
+        for (let [attr, value] of Object.entries(random_styles)) {
+            expected_styles += `${attr}: ${value}; `;
+        }
+        expected_styles = expected_styles.trim()
+        let expected_html = `<Div style="${expected_styles}"></Div>`;
+
+        const actual_html = div.html();
+        assert.equal(actual_html, expected_html, "Html generated with all attributes matches the expected output");
+    })
+
+    QUnit.test('Div.constructor', function (assert) {
+        const div = new Div()
+        assert.ok(div instanceof Div)
+        assert.equal(div.tag_name, 'Div')
+        assert.propEqual(div.styles, { background_color: null,
+                                       border          : null,
+                                       bottom          : null,
+                                       left            : null,
+                                       margin          : null,
+                                       overflow        : null,
+                                       padding         : null,
+                                       position        : null,
+                                       right           : null,
+                                       top             : null,
+                                       width           : null,
+                                       z_index         : null})
+
+        assert.equal(div.html(),'<Div></Div>')
+    });
+
+    QUnit.test('Div.set_value', function (assert) {
+        const div       =   new Div()
+        const div_styles = div.styles
+        div.set_style('top', '10px')
+        assert.equal(div_styles.top, '10px')
+    })
+
 
     QUnit.test('QUnit_Utils', function (assert) {
         assert.equal(this.qunit_utils.ping(), 'pong')
