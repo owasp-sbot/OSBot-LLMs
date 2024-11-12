48863a5659ff182a8e9edf57f8c773b6c0bbb968
Dinis Cruz <dinis.cruz@owasp.org>
2024-11-12 20:43:01 +0000

    minor formatting fix

--- tests/integration/fast_api/test_Fast_API__via_Http.py
+++ tests/integration/fast_api/test_Fast_API__via_Http.py
@@ -2,10 +2,7 @@ from unittest                                       import TestCase
 from fastapi                                        import FastAPI
 from osbot_fast_api.utils.Fast_API_Server           import Fast_API_Server
 from osbot_utils.context_managers.print_duration    import print_duration
-from osbot_utils.testing.Stdout import Stdout
-from osbot_utils.utils.Dev import pprint
-
-from osbot_llms.models.LLMs__Chat_Completion import LLMs__Chat_Completion
+from osbot_llms.models.LLMs__Chat_Completion        import LLMs__Chat_Completion
 from osbot_llms.utils.Version                       import version__osbot_llms
 from tests.llm_fast_api__for_tests                  import llm_fast_api, llm_fast_api__app
 
@@ -44,8 +41,6 @@ class test_Fast_API__via_Http(TestCase):
                           user_prompt  = '51-9'                     )
             llm_chat_completion = LLMs__Chat_Completion(**kwargs)
             response = self.fast_api_server.requests_post('/chat/completion', data=llm_chat_completion)
-            #pprint(dict(response.headers))
-            #pprint(response.text)
             assert '42' in response.text
 
 
